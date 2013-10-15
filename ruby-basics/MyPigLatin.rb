ee8e0ab976bbc367d44d6544a6d0c3f27ecb1e33
jhorsch <jhorsch@Jeffs-MacBook-Air.local>
2013-10-15 12:45:44 -0500

    submit hw

--- ruby-basics/MyPigLatin.rb
+++ ruby-basics/MyPigLatin.rb
@@ -1,22 +1,20 @@
 
 
-myphraselong = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal"
-myphrase = myphraselong.scan(/\w+/)
+myphraselong = "Four Score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal"
+# myphrase = myphraselong.scan(/\w+/)
+myphrase = myphraselong.split
 
-
-decodephrase = ""
 decodephraseNew = ""
-myphrase.each do |counter|
+myphrase.each do |every_word|
 
-
-
-  if  counter[0].downcase == 'a' || counter[0].downcase == 'e' || counter[0].downcase == 'i' || counter[0].downcase == 'o' || counter[0].downcase == 'u'
-      decodephrase = counter.downcase + "ay "
+  lower_every_word = every_word.downcase
+  if  lower_every_word[0] == 'a' || lower_every_word[0] == 'e' || lower_every_word[0] == 'i' || lower_every_word[0] == 'o' || lower_every_word[0] == 'u'
+      decodephrase = lower_every_word + "ay "
   else
-      decodephrase =  counter[1..-1] + counter[0].downcase + "ay "
+      decodephrase =  lower_every_word[1..-1] + lower_every_word[0] + "ay "
   end
 
-  decodephraseNew = decodephraseNew + decodephrase
+  decodephraseNew << decodephrase
 end
 
 puts decodephraseNew
